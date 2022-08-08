e00b92d931b252c7e23ffb43b00ab10763215532
Luis Tejada <luetejava@gmail.com>
2022-08-08 10:42:23 -0500

    creacion servicio para cars

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,19 +1,23 @@
 import { Controller, Get, Param } from '@nestjs/common';
+import { CarsService } from './cars.service';
 
 
 @Controller('cars')
 export class CarsController {
-    private cars = ['toyota','jac'];
+    
+    constructor(
+        private readonly carsService:CarsService
+    ){}
 
     @Get()
     getAllCArs(){
-        return this.cars;
+        return this.carsService.findAll();
     }
 
     @Get(':id')
     getCarById( @Param('id') id:string){
         console.log ({id})
-        return this.cars[id]; 
+        return this.carsService.findoneById( +id ); 
         
-    }
+     }
 }
---
+++ src/cars/cars.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class CarsService {
+    private cars = [
+        {
+        'id' : 1,
+        'brand' : 'toyota',
+        'model' : 'corolla'
+        
+    },
+    {
+        'id' : 2,
+        'brand' : 'jac',
+        'model' : 's2'
+        
+    },
+    {
+        'id' : 3,
+        'brand' : 'ford',
+        'model' : 'raptor'
+        
+    },
+];
+
+findAll(){
+    return this.cars
+}
+
+findoneById( id: number){
+    const car = this.cars.find( car=> car.id === id)
+    return car
+}
+
+}
