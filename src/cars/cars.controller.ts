16eb48b4c387fa8d4520fa067a2c58069cc36787
Luis Tejada <luetejava@gmail.com>
2022-08-08 10:08:48 -0500

    se actualiza cars

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,11 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 
 @Controller('cars')
 export class CarsController {
+    private cars = ['toyota','jac'];
 
     @Get()
     getAllCArs(){
-        return ['toyota','jac'];
+        return this.cars;
+    }
+
+    @Get(':id')
+    getCarById( @Param('id') id:string){
+        console.log ({id})
+        return this.cars[id]; 
+        
     }
 }
